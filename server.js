dde6f6f645e9404b6d378b533773ac2b35a11f8b
Gloria Vela <gloria.laura.vela@gmail.com>
2019-02-28 16:43:54 -0600

    create a post  message service

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Define the express here
 
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
     {name: 'Tim', message: 'Hola'},
@@ -12,6 +15,12 @@ var messages = [
 
 app.get('/messages', (req, res) =>{
     res.send(messages)
+})
+
+app.post('/messages', (req, res) =>{
+    //console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 var server = app.listen(8000, () => { 
